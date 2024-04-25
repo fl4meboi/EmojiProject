1173696785d2bd0217a8f668777ab7b6c1e2b415
fl4meboi <djkim9876@gmail.com>
2024-04-25 23:19:52 +0900

    KDJ: MqttManager, LoginPlayerController, LoginWidget, ErrorWidget, SaveGame, GameInstance class added

---
+++ Source/EmojiProject/Public/EmojiGameInstance.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "EmojiGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EMOJIPROJECT_API UEmojiGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+	
+};
